946a4e6ed2bebe4c49b04b6eead89df3698d8cab
Mage Narayan <sheshnarayan.iyer@gmail.com>
2024-12-30 04:55:02 +0530

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css"
+import { Inter } from 'next/font/google'
+import { cn } from "@/lib/utils"
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
+
